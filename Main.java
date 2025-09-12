8c16f50e754614f690958f3e49473c5c257b51d2
SNLVNX <tayfuremto@gmail.com>
2025-09-12 10:25:39 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    java.util.Scanner kb = new java.util.Scanner(System.in);
+	    int password;
+		System.out.println("Hoş geldiniz, lütfen ATM'ye yerleştirdiğiniz kartın 4 haneli şifresini giriniz...");
+		password = kb.nextInt();
+		System.out.println("");
+		System.out.println("");
+		
+		System.out.println("Hoş geldin Sinan Gülbaş, yapmak istediğin işlemi aşağıdan seçebilirsin.");
+		System.out.println("");
+		System.out.println("                                                                       Bakiyeniz : 77TL");
+		System.out.println("PARA ÇEKME");
+		System.out.println("PARA YATIRMA");
+		System.out.println("FATURA ÖDEME");
+		
+		
+	}
+}
